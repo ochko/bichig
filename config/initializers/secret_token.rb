10f5bebb51e937aebe4ff1ed489cd3ed254774ca
Ochirkhuyag <ochkoo@gmail.com>
2010-09-23 00:24:45 +0100

    new app generated

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bichig::Application.config.secret_token = 'ce9650adc4d10c39e82eb66ba79befc1d2464d854774083a3a5d0a0db95aa9fd09ac3af718925b334311862087b86d30bc6710b0cf82cee0067947ad523784fd'
