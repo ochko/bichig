cee4e4cdea4cd8ab4e41578c73ec3e5eb87df346
Ochirkhuyag <ochkoo@gmail.com>
2010-10-03 22:58:01 +0100

    because issue on the server, using temp file for pango-view.

--- app/models/otf_file.rb
+++ app/models/otf_file.rb
@@ -3,10 +3,8 @@ class OtfFile < ActiveRecord::Base
   SCRIPT_DIR = Rails.root.join('script')
   FEATURE_APPLY = 'changefeature.py'
   BASE_FONT = "#{Rails.root.join('public','system','template')}/template.ttf"
-  # Compile command should take 3 arguments below:
-  # compile_cmd base_font feature_file_name output_file_name
   COMPILE_CMD = "#{SCRIPT_DIR}/#{FEATURE_APPLY}"
-  RENDER_CMD = "/usr/bin/pango-view --font='%s' --rotate=90 --gravity=north --dpi=150 -q -t '%s' -o %s.png"
+  RENDER_CMD = "/usr/bin/pango-view --font='%s' --rotate=90 --gravity=north --dpi=150 -q -o %s.png %s"
 
   belongs_to :font, :class_name => OpenTypeFont.name
   has_many :rendered_examples, :foreign_key => 'file_id'
@@ -18,12 +16,15 @@ class OtfFile < ActiveRecord::Base
 
   def render_examples!
     imagesdir = "#{DIR}/#{self.version}"
+    txtfile = "#{imagesdir}/txt.tmp"
     FileUtils.mkdir(imagesdir) unless File.directory?(imagesdir)
     Example.all.each do |example|
-      cmd = RENDER_CMD % [self.font_name, example.mongolian, 
-                          "#{imagesdir}/#{example.id}"]
-      result = `#{cmd} 2>&1`
-      self.rendered_examples.find_or_create_by_example_id(example.id)
+      File.open(txtfile, 'w') { |f| f.write(example.mongolian) }
+      imagefile = "#{imagesdir}/#{example.id}"
+      cmd = RENDER_CMD % [self.font_name, imagefile, txtfile]
+      if system(cmd)
+        self.rendered_examples.find_or_create_by_example_id(example.id)
+      end
     end
     count_correctness!
   end
