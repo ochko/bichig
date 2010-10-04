94a1afcb76ecf7698b345c4cc2ee36c03bc6b0dc
Ochirkhuyag <ochkoo@gmail.com>
2010-10-05 00:21:14 +0100

    header for rendered examples

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def stack_glyphs(glyphs)
+    if glyphs
+      glyphs.split.map do |glyph|
+        image_tag("/images/glyphs/#{glyph}.png")
+      end
+    end
+  end
 end
